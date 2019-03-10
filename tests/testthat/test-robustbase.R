d7014187dc16775f60396b6bb80825b0abaef67b
Matt Lehman <mhlehman24@gmail.com>
2019-03-09 22:36:15 -0500

    fix documentation and tests

--- tests/testthat/test-robustbase.R
+++ tests/testthat/test-robustbase.R
@@ -66,5 +66,5 @@ test_that("augment.glmrob", {
   )
   
   # check that .se.fit column is included
-  check_dims(augment(fit2, se_fit = TRUE), 32, 5)
+  check_dims(augment(fit2, se_fit = TRUE), 32, 6)
 })
